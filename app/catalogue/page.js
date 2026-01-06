1f2ea2a31e3b4b8e6863fd3a1f9eae6e3a4e9795
steeveae <159084373+steeveae@users.noreply.github.com>
2026-01-06 19:56:47 +0100

    Create page.js

---
+++ app/catalogue/page.js
@@ -0,0 +1,8 @@
+export default function Catalogue() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Catalogue des vins 🍇</h1>
+      <p>Le catalogue connecté à Google Sheets sera affiché ici.</p>
+    </main>
+  )
+}
